a5c9890a9cdbd254a0cb0c1cf8e1870a511579e6
Imani Fecu <imani.fecu72@ymail.com>
2017-05-02 20:03:22 -0400

    seeded, created db and models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Meeting.create(
-  name: "First meeting with client",
-  address: "Orleans Plaza, 414 N Orleans St, Chicago, IL 60654",
-  start_time: DateTime.new(2016, 4, 26, 10),
-  end_time: DateTime.new(2016, 4, 26, 10),
-  notes: "Ugh single-origin coffee four loko, tote bag chartreuse skateboard disrupt truffaut cold-pressed deep v master cleanse mlkshk pop-up gluten-free yuccie. Bespoke pour-over four dollar toast health goth mustache, pabst whatever meh yuccie ugh meggings. Readymade tote bag tousled marfa, franzen sartorial keffiyeh cray tacos actually. Helvetica photo booth tumblr portland bespoke, pickled fap PBR&B raw denim. Yr seitan cronut neutra, taxidermy direct trade biodiesel helvetica. Before they sold out fixie pork belly, helvetica trust fund paleo craft beer swag marfa forage offal cronut. Paleo aesthetic chartreuse four loko ethical everyday carry, blue bottle tilde echo park direct trade."
+10.times do
+
+  Student.create(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  email: Faker::Internet.email,
+  phone_number: Faker::Number.number(10),
+  short_bio: Faker::Hipster.paragraph,
+  linkedin_url: "https://www.linkedin.com/in/brian-rice-95a672100/",
+  twitter_handle: "https://twitter.com/KennyKparekh04",
+  personal_web_url: "http://www.1happybirthday.com/HappyBirthday/Brian",
+  online_resume_url: "https://resumegenius.com/resume-templates",
+  github_url: "https://github.com/Hack-Slash",
+  photo: "http://www.warrenphotographic.co.uk/photography/cats/41427.jpg"
 )
+
+end
