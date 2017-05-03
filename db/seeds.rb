ccb37ec3dcaf8141e12ba2ccf3b9b97e9fe1a01a
Imani Fecu <imani.fecu72@ymail.com>
2017-05-02 20:40:41 -0400

    seed experiences

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do
+# 10.times do
 
-  Student.create(
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  email: Faker::Internet.email,
-  phone_number: Faker::Number.number(10),
-  short_bio: Faker::Hipster.paragraph,
-  linkedin_url: "https://www.linkedin.com/in/brian-rice-95a672100/",
-  twitter_handle: "https://twitter.com/KennyKparekh04",
-  personal_web_url: "http://www.1happybirthday.com/HappyBirthday/Brian",
-  online_resume_url: "https://resumegenius.com/resume-templates",
-  github_url: "https://github.com/Hack-Slash",
-  photo: "http://www.warrenphotographic.co.uk/photography/cats/41427.jpg"
-)
+#   Student.create(
+#   first_name: Faker::Name.first_name,
+#   last_name: Faker::Name.last_name,
+#   email: Faker::Internet.email,
+#   phone_number: Faker::Number.number(10),
+#   short_bio: Faker::Hipster.paragraph,
+#   linkedin_url: "https://www.linkedin.com/in/brian-rice-95a672100/",
+#   twitter_handle: "https://twitter.com/KennyKparekh04",
+#   personal_web_url: "http://www.1happybirthday.com/HappyBirthday/Brian",
+#   online_resume_url: "https://resumegenius.com/resume-templates",
+#   github_url: "https://github.com/Hack-Slash",
+#   photo: "http://www.warrenphotographic.co.uk/photography/cats/41427.jpg"
+# )
 
-end
+# end
+
+  Student.all.each do |student|
+    Experience.create(
+      student_id: student.id,
+      start_date: Faker::Date.between(2.days.ago, Date.today),
+      end_date: "CURRENT",
+      job_title: Faker::Job.title,
+      company_name: Faker::Company.name,
+      details: Faker::Company.bs,
+      )
+    end
